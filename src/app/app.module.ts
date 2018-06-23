b4d317b927e1395771991622879bc78e227f65e2
Porawit Poboonma <ball6847@gmail.com>
2018-06-24 01:10:12 +0700

    add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
